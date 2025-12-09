cf33416e236e9774445992eed2caab59ff11059f
KFANullSense <79997026+KFANullSense@users.noreply.github.com>
2025-12-09 16:05:30 +0000

    added navigation bar and main screens

--- LocalSocial/app/index.tsx
+++ LocalSocial/app/index.tsx
@@ -1,15 +1,84 @@
-import { Text, View } from "react-native";
+import BrowseScreen from '@/screens/Browse/BrowseScreen';
+import FriendsScreen from '@/screens/Friends/FriendsScreen';
+import HomeScreen from "@/screens/Home/HomeScreen";
+import MyEventsScreen from '@/screens/MyEvents/MyEventsScreen';
+import MyProfileScreen from '@/screens/Profile/MyProfileScreen';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function Index() {
+const TabNav = () => {
+  const Tab = createBottomTabNavigator();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+    <Tab.Navigator>
+      <Tab.Screen
+      name = "Profile"
+      component = {MyProfileScreen}
+      options = {{
+        tabBarIcon: ({color, size}) => (
+          <FontAwesome
+            name="user"
+            color={color}
+            size={size}/>
+        )
       }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      />
+      <Tab.Screen
+      name = "Friends"
+      component = {FriendsScreen}
+      options = {{
+        tabBarIcon: ({color, size}) => (
+          <FontAwesome
+            name="users"
+            color={color}
+            size={size}/>
+        )
+      }}
+      />
+      <Tab.Screen
+      name = "Home"
+      component = {HomeScreen}
+      options = {{
+        tabBarIcon: ({color, size}) => (
+          <MaterialCommunityIcons
+            name="home"
+            color={color}
+            size={size}/>
+        )
+      }}
+      />
+      <Tab.Screen
+      name = "My Events"
+      component = {MyEventsScreen}
+      options = {{
+        tabBarIcon: ({color, size}) => (
+          <FontAwesome5
+            name="folder-open"
+            color={color}
+            size={size}/>
+        )
+      }}
+      />
+      <Tab.Screen
+      name = "Browse"
+      component = {BrowseScreen}
+      options = {{
+        tabBarIcon: ({color, size}) => (
+          <FontAwesome5
+            name="map-marker-alt"
+            color={color}
+            size={size}/>
+        )
+      }}
+      />
+      
+    </Tab.Navigator>
+  )
+}
+
+export default function App() {
+  return (
+    <TabNav/>
   );
 }
